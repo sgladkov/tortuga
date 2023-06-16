4db2ae8517b1ea63d5ba6456e2777e9f3faa8d30
sgladkov <jahr@yandex.ru>
2023-06-16 17:59:30 +0300

    storage

---
+++ internal/storage/storage.go
@@ -0,0 +1,13 @@
+package storage
+
+import "github.com/sgladkov/tortuga/internal/models"
+
+type Storage interface {
+	GetUserList() (*models.UserList, error)
+	GetUser(id string) (*models.User, error)
+	GetProjectList() (*models.ProjectList, error)
+	GetUserProjects(userId string) (*models.ProjectList, error)
+	GetProject(id uint64) (*models.Project, error)
+	AddUser(user *models.User) error
+	UpdateUserNonce(id string, nonce uint64) error
+}
---
+++ internal/storage/teststorage.go
@@ -0,0 +1,84 @@
+package storage
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/sgladkov/tortuga/internal/models"
+)
+
+type TestStorage struct {
+	lock     sync.Mutex
+	Users    []models.User
+	Projects []models.Project
+	Bids     []models.Bid
+	Rates    []models.Rate
+}
+
+func NewTestStorage(users []models.User, projects []models.Project, bids []models.Bid, rates []models.Rate) *TestStorage {
+	return &TestStorage{
+		Users:    users,
+		Projects: projects,
+		Bids:     bids,
+		Rates:    rates,
+	}
+}
+
+func (t TestStorage) GetUserList() (*models.UserList, error) {
+	res := models.UserList{}
+	for _, u := range t.Users {
+		res = append(res, u.Id)
+	}
+	return &res, nil
+}
+
+func (t TestStorage) GetUser(id string) (*models.User, error) {
+	for _, u := range t.Users {
+		if u.Id == id {
+			return &u, nil
+		}
+	}
+	return nil, fmt.Errorf("no user with id %s", id)
+}
+
+func (t TestStorage) GetProjectList() (*models.ProjectList, error) {
+	res := models.ProjectList{}
+	for _, p := range t.Projects {
+		res = append(res, p.Id)
+	}
+	return &res, nil
+}
+
+func (t TestStorage) GetUserProjects(userId string) (*models.ProjectList, error) {
+	res := models.ProjectList{}
+	for _, p := range t.Projects {
+		if p.Owner == userId {
+			res = append(res, p.Id)
+		}
+	}
+	return &res, nil
+}
+
+func (t TestStorage) GetProject(id uint64) (*models.Project, error) {
+	for _, p := range t.Projects {
+		if p.Id == id {
+			return &p, nil
+		}
+	}
+	return nil, fmt.Errorf("no project with id %v", id)
+}
+
+func (t TestStorage) AddUser(user *models.User) error {
+	t.Users = append(t.Users, *user)
+	return nil
+}
+
+func (t TestStorage) UpdateUserNonce(id string, nonce uint64) error {
+	for _, u := range t.Users {
+		if u.Id == id {
+			u.Nonce = nonce
+			return nil
+		}
+	}
+	return fmt.Errorf("no user with id %s", id)
+}
